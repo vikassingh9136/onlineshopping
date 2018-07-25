be0f9242c30b8683e45fd9329939f04975819c5b
vikassingh9136 <vikassingh9136@users.noreply.github.com>
2018-07-25 18:28:50 +0530

    My Devops Project 1
    My Devops Project 1

---
+++ shopingbackend/src/main/java/com/shopingbackend/App.java
@@ -0,0 +1,13 @@
+package com.shopingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
